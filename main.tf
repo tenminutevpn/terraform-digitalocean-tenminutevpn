d84c2ecd001e5c467f376a037fbb28a5f6de12b0
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-04 01:17:09 +0400

    updated the url in digitalocean_custom_image to use tenminutevpn.raw.gz

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "digitalocean_vpc" "this" {
 
 resource "digitalocean_custom_image" "this" {
   name         = "tenminutevpn"
-  url          = one([for item in data.github_release.this.assets : item if item.name == "debian-12.raw.gz"]).browser_download_url
+  url          = one([for item in data.github_release.this.assets : item if item.name == "tenminutevpn.raw.gz"]).browser_download_url
   distribution = "Debian"
   regions      = [data.digitalocean_region.this.slug]
 
