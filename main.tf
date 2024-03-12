e513ffb47835fb0ce21bba4b2d68a6e2aed68ec5
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-13 03:13:13 +0400

    added tcp and icmp inbound rules to digitalocean firewall configuration

--- main.tf
+++ main.tf
@@ -65,6 +65,17 @@ resource "digitalocean_firewall" "this" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "3128"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
